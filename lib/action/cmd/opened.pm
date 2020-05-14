07e78228458e2c19196e0b13ad99fdaa503caa20
Nicolas R <cpan@atoomic.org>
2020-05-14 10:54:00 -0600

    cleanup

--- lib/action/cmd/opened.pm
+++ lib/action/cmd/opened.pm
@@ -19,44 +19,11 @@ sub run($action) {
     if ( $action->is_maintainer ) {
         $action->rebase_and_merge;
     }
-
-    return 1;    # FIXME to remove - always fail for now so we can restart the workflow
+    else {
+        ...;
+    }
 
     return;
 }
 
 1;
-
-__END__
-
-function closeOnFailure () {
-    # neutral, success, cancelled, timed_out, failure
-    if [ "$WORKFLOW_CONCLUSION" != 'success' ] ; then
-        addComment $PR_NUMBER "**Automatically closing** the Pull Request on failure: workflow conclusion was **$WORKFLOW_CONCLUSION**"
-        closePR $PR_NUMBER
-        exit 0
-    fi
-}
-
-function check_openPullRequest () {
-
-    closeOnFailure # and exit
-
-    # the PR is clean
-    local pr_from_maintainer
-    pr_from_maintainer=$(isPRFromMaintainer "$PR_NUMBER")
-
-    if [ "$pr_from_maintainer" == true ]; then
-        addComment $PR_NUMBER "**Clean PR** from Maintainer merging to $TARGET_BRANCH branch"
-
-        git rebase origin/$TARGET_BRANCH
-        git push origin HEAD:$TARGET_BRANCH
-
-        #git checkout -b origin/p5
-        #git merge $HEAD_SHA --no-edit
-        #git push origin HEAD:p5
-        # PR should be auto closed...
-    else
-        addComment $PR_NUMBER "Needs Code Review from Maintainers"
-    fi
-}
